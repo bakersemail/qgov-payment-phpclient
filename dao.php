15e0417bc5582418ffd66e66994a23a2c092c5a9
bakersemail <bakersemail@hotmail.com>
2013-12-23 15:44:55 +1000

    using 2 spaces instead of tabs for readability

--- dao.php
+++ dao.php
@@ -1,15 +1,15 @@
 <?php
-	function findByServiceId($serviceId) {
-		return '110018'; #a known paid generatedOrderId for testing
-	}
-	
-	function findByOrderId($orderId) {
-	}
-	
-	function saveService($serviceId, $orderId) {
-	}
-	
-	function createServiceId() {
-		return '123';
-	}
+  function findByServiceId($serviceId) {
+    return '110018'; #a known paid generatedOrderId for testing
+  }
+  
+  function findByOrderId($orderId) {
+  }
+  
+  function saveService($serviceId, $orderId) {
+  }
+  
+  function createServiceId() {
+    return '123';
+  }
 ?>
