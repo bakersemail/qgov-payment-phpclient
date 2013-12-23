7ade217d0c42dbbb17ca8506385b7a7c1d7865af
Steven Baker <bakersemail@hotmail.com>
2013-12-23 22:02:30 +1000

    logging to help debugging

--- dao.php
+++ dao.php
@@ -2,14 +2,19 @@
   require_once 'sqlitedao.php';
   
   function findByServiceId($serviceId) {
-    return _findByServiceId($serviceId);
+    $result = _findByServiceId($serviceId);
+	error_log("Found Order ID: $result from Service ID: $serviceId");
+	return $result;
   }
   
   function saveService($serviceId, $orderId) {
+	error_log("Saving Service ID: $serviceId / Order ID: $orderId");
     return _saveService($serviceId, $orderId);
   }
   
   function createServiceId() {
-    return _createServiceId();
+	$result = _createServiceId();
+	error_log("Creating Service ID: $result");
+    return $result;
   }
 ?>
